6184f60fdef3d82e3393efb3da29dd54908cc64c
theslayer04 <m.omar20123@gmail.com>
2014-11-17 10:24:05 +0200

    hey

--- lolo.py
+++ lolo.py
@@ -1,4 +1,26 @@
-x = [3,3,4,5,7,64,664,46,234]
-x.insert(3,4)
-print(x)
-x = input()
+filew = open('numbers.txt','w')
+#user input
+uinput = ''
+count = 0
+total = 0
+
+#loop to get inputs
+while True:
+    uinput = input('Enter your numbers(press E to Exit):')
+    if uinput == str():
+        if uinput != 'e':
+           break
+        else:
+            print ('Please Enter numbers only')
+            continue 
+    else:
+       write = filew.write(uinput + '\n')
+       count = count + 1
+       total = total + int(uinput)
+
+print ('Thank you for using my app')
+
+Average = total / count
+      
+filew.write('Average is: '+ str(Average))
+print ('the average is: ' + str(Average))
