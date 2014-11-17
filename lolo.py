d5d0e26d0ba6be9b86de9ce1b344206b622cc37e
theslayer04 <m.omar20123@gmail.com>
2014-11-18 00:16:25 +0200

    another test

--- lolo.py
+++ lolo.py
@@ -1,26 +1,10 @@
-filew = open('numbers.txt','w')
-#user input
-uinput = ''
-count = 0
-total = 0
+input1 = int(input())
 
-#loop to get inputs
-while True:
-    uinput = input('Enter your numbers(press E to Exit):')
-    if uinput == str():
-        if uinput != 'e':
-           break
-        else:
-            print ('Please Enter numbers only')
-            continue 
-    else:
-       write = filew.write(uinput + '\n')
-       count = count + 1
-       total = total + int(uinput)
+input2 = int(input())
+try:
+    result = input1 / input2
+    print(result)
+except ZeroDivisionError:
+    print("You are officially an idiot ")
 
-print ('Thank you for using my app')
 
-Average = total / count
-      
-filew.write('Average is: '+ str(Average))
-print ('the average is: ' + str(Average))
